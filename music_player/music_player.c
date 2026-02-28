7db3f4898c1ff8f1e2b8595a5e6de260b1160e92
H4W9 <hoferwade@gmail.com>
2026-02-27 19:06:22 -0600

    Update music_player.c

--- music_player/music_player.c
+++ music_player/music_player.c
@@ -11,7 +11,7 @@
 #define TAG "MusicPlayer"
 
 #define MUSIC_PLAYER_APP_EXTENSION "*"
-#define MUSIC_PLAYER_EXAMPLE_FILE  "Marble_Machine.fmf"
+#define MUSIC_PLAYER_EXAMPLE_FILE "Marble_Machine.fmf"
 
 #define MUSIC_PLAYER_SEMITONE_HISTORY_SIZE 3
 
